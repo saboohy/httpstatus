864060cd7cc3c373652a490c88f30a2b5d3c3da8
Sabuhi Alizada <sabuhi.alizada@yahoo.com>
2022-07-24 15:59:00 +0400

    Add HttpException

---
+++ src/HttpException.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace Saboohy\HttpStatus
+{
+    use Saboohy\HttpStatus\Message;
+
+    /**
+     * HttpException handler
+     * 
+     * @package     Saboohy\HttpStatus
+     * @subpackage  HttpException
+     * @author      Sabuhi Alizada <sabuhi.alizada@yahoo.com>
+     * @license     MIT
+     */
+    class HttpException extends \Exception
+    {
+        /**
+         * Constructor
+         * 
+         * @param int $http_status_code
+         * 
+         * @return void
+         */
+        public function __construct(int $http_status_code)
+        {
+            $message = Message::get($http_status_code);
+            parent::__construct($message, $http_status_code);
+        }
+    }
+}
