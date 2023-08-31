a25c0551b2b5cfd04a37f811a6a061b650899338
saboohy <sabuhi.alizada@yahoo.com>
2023-08-31 15:15:20 +0400

    Convert from class to enum

--- src/Server.php
+++ src/Server.php
@@ -2,6 +2,8 @@
 
 namespace Saboohy\HttpStatus;
 
+use Saboohy\HttpStatus\EnumReflector;
+
 /**
  * Server error responses
  * 
@@ -10,19 +12,45 @@ namespace Saboohy\HttpStatus;
  * @author      Sabuhi Alizada <sabuhi.alizada@yahoo.com>
  * @license     MIT
  */
-final class Server
+enum Server: int
 {
-    public const INTERNAL_SERVER_ERROR = 500;
-    public const NOT_IMPLEMENTED = 501;
-    public const BAD_GATEWAY = 502;
-    public const SERVICE_UNAVAILABLE = 503;
-    public const GATEWAY_TIMEOUT = 504;
-    public const HTTP_VERSION_NOT_SUPPORTED = 505;
-    public const VARIANT_ALSO_NEGOTIATES = 506;
-    public const INSUFFICIENT_STORAGE = 507;
-    public const LOOP_DETECTED = 508;
-    public const NOT_EXTENDED = 510;
-    public const NETWORK_AUTHENTICATION_REQUIRED = 511;
-    public const NETWORK_READ_TIMEOUT_ERROR = 598;
-    public const NETWORK_CONNECT_TIMEOUT_ERROR = 599;
+    use EnumReflector;
+
+    case INTERNAL_SERVER_ERROR = 500;
+    case NOT_IMPLEMENTED = 501;
+    case BAD_GATEWAY = 502;
+    case SERVICE_UNAVAILABLE = 503;
+    case GATEWAY_TIMEOUT = 504;
+    case HTTP_VERSION_NOT_SUPPORTED = 505;
+    case VARIANT_ALSO_NEGOTIATES = 506;
+    case INSUFFICIENT_STORAGE = 507;
+    case LOOP_DETECTED = 508;
+    case NOT_EXTENDED = 510;
+    case NETWORK_AUTHENTICATION_REQUIRED = 511;
+    case NETWORK_READ_TIMEOUT_ERROR = 598;
+    case NETWORK_CONNECT_TIMEOUT_ERROR = 599;
+
+    /**
+     * Returns message by case
+     * 
+     * @return string
+     */
+    public function message(): string
+    {
+        return match($this) {
+            Server::INTERNAL_SERVER_ERROR => "Internal Server Error",
+            Server::NOT_IMPLEMENTED => "Not Implemented",
+            Server::BAD_GATEWAY => "Bad Gateway",
+            Server::SERVICE_UNAVAILABLE => "Service Unavailable",
+            Server::GATEWAY_TIMEOUT => "Gateway Timeout",
+            Server::HTTP_VERSION_NOT_SUPPORTED => "HTTP Version Not Supported",
+            Server::VARIANT_ALSO_NEGOTIATES => "Variant Also Negotiates",
+            Server::INSUFFICIENT_STORAGE => "Insufficient Storage",
+            Server::LOOP_DETECTED => "Loop Detected",
+            Server::NOT_EXTENDED => "Not Extended",
+            Server::NETWORK_AUTHENTICATION_REQUIRED => "Network Authentication Required",
+            Server::NETWORK_READ_TIMEOUT_ERROR => "Network Read Timeout Error",
+            Server::NETWORK_CONNECT_TIMEOUT_ERROR => "Network Connect Timeout Error"
+        };
+    }
 }
